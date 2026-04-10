63320527fe8b800fc36ecf5e72926186d7ffdbcf
Vainq-555 <vainqueurmazibo@gmail.com>
2026-04-09 20:39:55 -0500

    Add PWA support

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'my-site-v1';
+const urlsToCache = [
+  '/',
+  '/style.css',
+  '/script.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) return response;
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200) return response;
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        });
+      })
+  );
+});
